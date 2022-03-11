39931515dda4cfb8a93e12bbfcaeee953e5eb1ef
jayson <leobest9688@gmail.com>
2022-03-11 23:49:31 +0800

    add copy to clipboard function

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -7,7 +7,8 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#FF6700').all(10))
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
