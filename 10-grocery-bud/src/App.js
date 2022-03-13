ca174371f152ce74783628c92c092c9b3f590997
jayson <leobest9688@gmail.com>
2022-03-13 23:55:02 +0800

    add items

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -4,19 +4,29 @@ import Alert from './Alert'
 
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: true, msg: '', type: '' });
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello')
-  }
+    if (!name) {
+
+    } else if (name && isEditing) {
+      
+    } else {
+      const newItem = { id: new Date().getTime().toString(), input: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
 
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
         {alert.show && <Alert />}
+
         <h3>grocery bud</h3>
         <div className='form-control'>
           <input
@@ -31,15 +41,16 @@ function App() {
           </button>
         </div>
       </form>
-
-      <div className='grocery-container'>
-        <List />
-        <button className='clear-btn'>
-          clear items
-        </button>
-      </div>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>
+            clear items
+          </button>
+        </div>
+      )}
     </section>
-  )
+  );
 }
 
 export default App
