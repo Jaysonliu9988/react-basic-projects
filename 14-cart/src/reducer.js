44904e32b7536473961db39ddb3d096b7e55a8a9
jayson <leobest9688@gmail.com>
2022-08-14 22:58:42 +0800

    fetch data

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -47,12 +47,16 @@ const reducer = (state, action) => {
             amount: 0,
         })
         total = parseFloat(total.toFixed(2))
-
-
         return { ...state, total, amount }
     }
 
+    if (action.type === 'LOADING') {
+        return { ...state, loading: true }
+    }
 
+    if (action.type === 'DISPLAY_ITEMS') {
+        return { ...state, cart: action.payload, loading: false }
+    }
 
 
 
