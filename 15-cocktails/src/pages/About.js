7a529b9e49aa92104b7d09a714f7315aa75d1e38
jayson <leobest9688@gmail.com>
2022-08-15 00:31:40 +0800

    link component

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section">
+      <h2>About page</h2>
+      <Link to='/' className='btn'>
+        Home
+      </Link>
+    </section>
   )
 }
 
--- 15-cocktails/src/pages/Home.js
+++ 15-cocktails/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
 
 const Home = () => {
   return (
     <div>
-      <h2>home page</h2>
+      <h2 className='section'>Home page</h2>
+      <Link to='/about' className='btn'>
+        About
+      </Link>
     </div>
   )
 }
