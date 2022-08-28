a9b0ba59a9a5cb8ca3b9dd9d49a86996c1f9c07f
jayson <leobest9688@gmail.com>
2022-08-28 22:40:32 +0800

    add error page

--- 15-cocktails/src/pages/Home.js
+++ 15-cocktails/src/pages/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
+import SingleCocktail from './SingleCocktail'
 
 const Home = () => {
   return (
     <main>
       <SearchForm />
+      <SingleCocktail />
     </main>
   )
 }
